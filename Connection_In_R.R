2cb1dbf939fb1cb13ee833ad7ee90401e79de1ae
Dhiraj <dhiraj.jamia@gmail.com>
2018-12-03 00:05:07 +0530

    Update Connection_In_R.R

--- Connection_In_R.R
+++ Connection_In_R.R
@@ -20,4 +20,21 @@ airlines <- copy_to(sc, airlines, "airlines")
 View(airlines)
 src_tbls(sc)
 select(flights, year:day, arr_delay, dep_delay)
-
+filter(flights, dep_delay > 1000)
+arrange(flights, desc(dep_delay))
+summarise(flights, mean_dep_delay = mean(dep_delay))
+mutate(flights, speed = distance / air_time * 60)
+c1 <- filter(flights, day == 17, month == 5, carrier %in% c('UA', 'WN', 'AA', 'DL'))
+c2 <- select(c1, year, month, day, carrier, dep_delay, air_time, distance)
+c3 <- arrange(c2, year, month, day, carrier)
+c4 <- mutate(c3, air_time_hours = air_time / 60)
+c4 <- flights %>%
+  filter(month == 5, day == 17, carrier %in% c('UA', 'WN', 'AA', 'DL')) %>%
+  select(carrier, dep_delay, air_time, distance) %>%
+  arrange(carrier) %>%
+  mutate(air_time_hours = air_time / 60)
+c4 %>%
+  group_by(carrier) %>%
+  summarize(count = n(), mean_dep_delay = mean(dep_delay))
+carrierhours <- collect(c4)
+with(carrierhours, pairwise.t.test(air_time, carrier))
