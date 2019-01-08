d1287cca1cb9dcd9904104e60c75a744715a23af
Dhiraj <dhiraj.jamia@gmail.com>
2019-01-08 16:25:40 +0530

    Update Read_Write.R

--- Read_Write.R
+++ Read_Write.R
@@ -18,3 +18,41 @@ spark_write_json(iris_tbl, temp_json)
 iris_json_tbl <- spark_read_json(sc, "iris_json", temp_json)
 
 src_tbls(sc)
+
+#Extensions
+#The facilities used internally by sparklyr for its dplyr and machine learning interfaces are available to extension packages.
+#Since Spark is a general purpose cluster computing system there are many potential applications for extensions 
+#(e.g. interfaces to custom machine learning pipelines, interfaces to 3rd party Spark packages, etc.).
+
+#Here’s a simple example that wraps a Spark text file line counting function with an R function:
+
+# write a CSV 
+tempfile <- tempfile(fileext = ".csv")
+write.csv(nycflights13::flights, tempfile, row.names = FALSE, na = "")
+
+# define an R interface to Spark line counting
+count_lines <- function(sc, path) {
+  spark_context(sc) %>% 
+    invoke("textFile", path, 1L) %>% 
+      invoke("count")
+}
+
+# call spark to count the lines of the CSV
+count_lines(sc, tempfile)
+
+
+
+#Table Utilities
+#You can cache a table into memory with:
+tbl_cache(sc, "batting") #where batting is a file name 
+#and unload from memory using:
+
+tbl_uncache(sc, "batting") #where batting is a file name 
+
+#Connection Utilities
+#You can view the Spark web console using the spark_web function:
+spark_web(sc)
+#You can show the log using the spark_log function:
+spark_log(sc, n = 10)
+
+spark_disconnect(sc) # disconnect connection
