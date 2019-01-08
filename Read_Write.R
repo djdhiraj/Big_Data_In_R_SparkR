643f5ba6f4ee3a8dfd3e26c4497ebe6d1beae353
Dhiraj <dhiraj.jamia@gmail.com>
2019-01-08 16:13:54 +0530

    Create Read_Write.R

---
+++ Read_Write.R
@@ -0,0 +1,20 @@
+#Reading and Writing Data
+
+#You can read and write data in CSV, JSON, and Parquet formats. Data can be stored in HDFS, S3, 
+#or on the lcoal filesystem of cluster nodes.
+
+
+temp_csv <- tempfile(fileext = ".csv")
+temp_parquet <- tempfile(fileext = ".parquet")
+temp_json <- tempfile(fileext = ".json")
+
+spark_write_csv(iris_tbl, temp_csv)
+iris_csv_tbl <- spark_read_csv(sc, "iris_csv", temp_csv)
+
+spark_write_parquet(iris_tbl, temp_parquet)
+iris_parquet_tbl <- spark_read_parquet(sc, "iris_parquet", temp_parquet)
+
+spark_write_json(iris_tbl, temp_json)
+iris_json_tbl <- spark_read_json(sc, "iris_json", temp_json)
+
+src_tbls(sc)
